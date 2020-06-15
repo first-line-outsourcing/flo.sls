a5e93cc796b50ac80f96a898dffe578a9687ccae
azhivaeva-flo <azhivaeva@flo.team>
2020-06-15 10:32:19 +0300

    Added createIconikService

--- helper/helper.ts
+++ helper/helper.ts
@@ -1,3 +1,17 @@
+import { IconikService } from '@workflowwin/iconik-api';
+
+export function createIconikService(options?, request?) {
+  const iconikOptions = {
+    appId: process.env.ICONIK_APP_ID as string,
+    authToken: request ? request.headers['Auth-Token'] || request.headers['auth-token'] : process.env.ICONIK_TOKEN,
+    iconikUrl: process.env.ICONIK_URL as string,
+    systemDomainId: process.env.ICONIK_SYSTEM_DOMAIN_ID,
+    ...options,
+  };
+
+  return new IconikService(iconikOptions);
+}
+
 export function arrayToObject(array: any[], field: string): any {
   if (!array || !array.length) {
     return {};
