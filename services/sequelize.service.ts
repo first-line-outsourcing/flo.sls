eeb79eb9bf2e1afc74666efd72bf55e52e3b675c
azhivaeva <azhivaeva@bestmood.tech>
2020-06-25 10:57:36 +0300

    - Updated sequelize.service

--- services/sequelize.service.ts
+++ services/sequelize.service.ts
@@ -3,9 +3,11 @@ import * as models from '../models/PostgreSQL';
 
 const connection: { isConnected?: boolean } = {};
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-export const connect = async context => {
-  context.callbackWaitsForEmptyEventLoop = false;
+/**
+ * context.callbackWaitsForEmptyEventLoop = false should be set only for non-async handlers
+ * for more information, read this https://docs.amazonaws.cn/en_us/lambda/latest/dg/nodejs-handler.html
+ */
+export const connect = async () => {
   if (connection.isConnected) {
     return models;
   }
