7ddd4650e62be731c3b2b8e86da94dea828fa139
emahota-flo <emahota@flo.team>
2021-04-13 17:10:32 +0300

    RCF-2 #comment fix margin

--- errors/runtime/index.ts
+++ errors/runtime/index.ts
@@ -3,5 +3,5 @@ export { InputValidationError } from './input-validation-error';
 /**
  * Example of usage:
  * if (!mediaInfoUrl.url)
- throw new InputValidationError("The param 'url' is required.");
+ *    throw new InputValidationError("The param 'url' is required.");
  * */
