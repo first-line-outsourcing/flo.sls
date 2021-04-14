143f17b38c3abcd7d0758c10bdb7bdda530f4f3e
emahota-flo <emahota@flo.team>
2021-04-14 12:22:08 +0300

    RFC-2 #comment added ErrorStatusCode

--- errors/http/http-error.ts
+++ errors/http/http-error.ts
@@ -1,5 +1,7 @@
+export type ErrorStatusCode = 400 | 401 | 403 | 404 | 409 | 422 | 429 | 500 | 502 | 503 | 504;
+
 export class HttpError extends Error {
-  constructor(public statusCode: number, public name: string, message: string) {
+  constructor(public statusCode: ErrorStatusCode, public name: string, message: string) {
     super(message);
     Object.setPrototypeOf(this, new.target.prototype); // Restore prototype chain
     Error.captureStackTrace(this);
