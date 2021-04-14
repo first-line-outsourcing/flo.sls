d57dadaa97b39064a22c2066654e93a90307f3f2
emahota-flo <emahota@flo.team>
2021-04-14 14:35:08 +0300

    RFC-2 #comment fix comments: added method for format unknown errors, refactored in error handler

--- errors/http/http-error.ts
+++ errors/http/http-error.ts
@@ -3,7 +3,9 @@ export type ErrorStatusCode = 400 | 401 | 403 | 404 | 409 | 422 | 429 | 500 | 50
 export class HttpError extends Error {
   constructor(public statusCode: ErrorStatusCode, public name: string, message: string) {
     super(message);
-    Object.setPrototypeOf(this, new.target.prototype); // Restore prototype chain
+    if (Object.getPrototypeOf(this) === new.target.prototype) {
+      Object.setPrototypeOf(this, new.target.prototype); // Restore prototype chain
+    }
     Error.captureStackTrace(this);
   }
 }
--- errors/runtime/runtime-error.ts
+++ errors/runtime/runtime-error.ts
@@ -1,7 +1,8 @@
 export class RuntimeError extends Error {
   constructor(message: string) {
     super(message);
-    Object.setPrototypeOf(this, new.target.prototype); // Restore prototype chain
-    Error.captureStackTrace(this);
+    if (Object.getPrototypeOf(this) === new.target.prototype) {
+      Object.setPrototypeOf(this, new.target.prototype); // Restore prototype chain
+    }
   }
 }
