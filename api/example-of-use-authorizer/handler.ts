48b4baf3703efa514761a014276ce0e46889fde2
emahota-flo <emahota@flo.team>
2021-04-19 18:15:19 +0300

    IPD-40 #comment added comment for example lambda

--- api/example-of-use-authorizer/handler.ts
+++ api/example-of-use-authorizer/handler.ts
@@ -6,7 +6,12 @@ import { Handler } from 'aws-lambda';
 export const exampleOfUseAuthorizer: Handler<APIGatewayLambdaEvent<{ message: string }>> = async (event) => {
   log('[Examples] Example of use Iconik Authorizer', event);
   try {
-    return { message: 'test' };
+    /**
+     * @enhancedAuthContext contains information about user and Iconik credentials
+     * */
+    const { body, enhancedAuthContext } = event;
+
+    return { message: 'Magic unicorns are better than humans!' };
   } catch (error) {
     errorHandler(error);
   }
