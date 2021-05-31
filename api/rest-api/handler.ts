3b1c9c5bf9c46e401c520abc8f78a0dd0d3c7e98
Kirill Khoroshilov <khoroshilov@flo.team>
2021-05-31 16:23:58 +0300

    chore: short paths

--- api/rest-api/handler.ts
+++ api/rest-api/handler.ts
@@ -2,8 +2,8 @@
 
 import { log } from '@helper/logger';
 import { Handler } from 'aws-lambda';
-import { errorHandler } from '../../helper/rest-api/error-handler';
-import { APIGatewayLambdaEvent } from '../../interfaces/api-gateway-lambda.interface';
+import { errorHandler } from '@helper/rest-api/error-handler';
+import { APIGatewayLambdaEvent } from '@interfaces/api-gateway-lambda.interface';
 
 export const handler: Handler<APIGatewayLambdaEvent<null>, string> = async (event) => {
   log(event);
